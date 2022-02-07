c0bf4fd524bda739f399258b62eecc653ec2f050
Piyush Singh <65072631+singhpiyush998@users.noreply.github.com>
2022-02-07 18:17:46 +0000

    6th one done

--- p6original.c
+++ p6original.c
@@ -8,9 +8,30 @@ void input_string(char *a)
 
 void str_reverse(char *str, char *rev_str)
 {
-  int n = 
-  for (int i = 0; str[i], i++)
+  int n;
+  for (n = 0; str[n]; n++);
+  n--;
+  int i;
+  for (i = 0; str[i]; i++)
   {
-
+    rev_str[i] = str[n];
+    n--;
   }
+  rev_str[i] = '\0';
 }
+
+void output(char *a, char *reverse_a)
+{
+  printf("The reverse of %s is %s\n", a, reverse_a);
+}
+
+int main()
+{
+  char str[10];
+  input_string(str);
+  char rev_str[10];
+  str_reverse(str, rev_str);
+  output(str, rev_str);
+  return 0;
+}
+
